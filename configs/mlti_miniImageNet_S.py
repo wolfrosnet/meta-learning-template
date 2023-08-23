0ca52837d2d37d0bd9bf67dcdc870be395644af7
Mingi Hong <mhhong@hanyang.ac.kr>
2023-08-23 14:25:57 +0900

    0823

--- configs/mlti_miniImageNet_S.py
+++ configs/mlti_miniImageNet_S.py
@@ -12,7 +12,7 @@ class Config:
     fast_lr=0.01 # For MiniImagenet, both models were trained using 5 gradient steps of size α = 0.01, 
     meta_batch_size=4 # We used a meta batch-size of 4 and 2 tasks for 1-shot and 5-shot training respectively.
     adaptation_steps=5 # For MiniImagenet, both models were trained using 5 gradient steps of size α = 0.01, 
-    num_iterations=60000 # All models were trained for 60000 iterations on a single NVIDIA Pascal Titan X GPU.
+    num_iterations=20000 # All models were trained for 60000 iterations on a single NVIDIA Pascal Titan X GPU.
     seed=42
     time_now = dt.datetime.now()
     run_id = time_now.strftime("%m%d%H%M%S")
@@ -27,5 +27,5 @@ class Config:
     z_dim=32
     use_wandb=True
     mixup_method='cutmix'
-    test_ckpt_path='./checkpoint/mlti_miniImageNet-S_0821151020/best.pt'
+    test_ckpt_path='./checkpoint/mlti_miniImageNet-S_0822124830/best.pt'
     num_test_points=600
